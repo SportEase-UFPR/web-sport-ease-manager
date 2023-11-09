434261a52745d686c4272bef957820f619d3091a
MatheusKerscher <kerschermatheus12@gmail.com>
2023-11-09 20:45:40 -0300

    Alteração da api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://api-gateway-production-75aa.up.railway.app/',
+  baseUrl: 'https://api-gateway-production-40a1.up.railway.app/',
   email: 'sportease.ufpr@gmail.com',
   ss_token: 'SS_ADM',
   userProfile: 'ROLE_ADM',
